161b1177c5c285151bdb2d651296740eb9722a76
Mahmoud H. Alim <mahmoud.hussein@gen-c.com>
2022-02-06 16:20:05 +0200

    welcome screen (#5)
    * add first screen composable UI , add new ActionButton.kt ,
    
    * edit app theme and Fonts, add Dimensions.kt data class to contain all needed Dimensions.kt to be used in spacing by app theme
    
    * add UiEvent Sealed class
    
    to handle UiEvents (navigation for now), pass onNavigate Callback to  WelcomeScreen.kt to prevent passing down the NavController
    
    * add Route.kt
    
    define all app navigation routes in core moduule
    
    * add NavControllerExt.kt
    
    add an extention navigate() fun on NavController to navigate with a UiEvent
    
    * add navHost with Route.WELCOME start des and add all other destinations inside of main activity, add on navigate callback in WelcomeScreen.kt to navigate to next screen

--- app/src/main/java/com/mahmoudalim/calorytrackingapp/ui/theme/Type.kt
+++ app/src/main/java/com/mahmoudalim/calorytrackingapp/ui/theme/Type.kt
@@ -9,49 +9,6 @@ import androidx.compose.ui.unit.sp
 import com.mahmoudalim.calorytrackingapp.R
 
 // Set of Material typography styles to start with
-val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    ),
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 20.sp,
-    ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    ),
-    h1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 30.sp
-    ),
-    h2 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 26.sp
-    ),
-    h3 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp
-    ),
-    h4 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 18.sp
-    ),
-    h5 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 14.sp
-    ),
-)
-
 
 val Roboto = FontFamily(
     Font(R.font.roboto_light, FontWeight.Light),
@@ -59,3 +16,48 @@ val Roboto = FontFamily(
     Font(R.font.roboto_medium, FontWeight.Medium),
     Font(R.font.rboto_bold, FontWeight.Bold)
 )
+
+val Typography = Typography(
+    body1 = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    ),
+    button = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.W500,
+        fontSize = 20.sp,
+    ),
+    caption = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
+    ),
+    h1 = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.Normal,
+        fontSize = 30.sp
+    ),
+    h2 = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.Normal,
+        fontSize = 26.sp
+    ),
+    h3 = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.Normal,
+        fontSize = 22.sp
+    ),
+    h4 = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.Normal,
+        fontSize = 18.sp
+    ),
+    h5 = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
+    ),
+)
+
+
