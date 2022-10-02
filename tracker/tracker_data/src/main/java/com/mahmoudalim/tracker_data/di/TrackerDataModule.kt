bd29a3c411fe3c485fa280f8155b7ee80ec06c16
Mahmoud H. Alim <mahmoudhusseinalim@gmail.com>
2022-10-02 16:34:56 +0200

    implement TrackerData di module

---
+++ tracker/tracker_data/src/main/java/com/mahmoudalim/tracker_data/di/TrackerDataModule.kt
@@ -0,0 +1,42 @@
+package com.mahmoudalim.tracker_data.di
+
+import com.mahmoudalim.tracker_data.remote.api.OpenFoodApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
+
+/**
+ * @author Mahmoud Alim on 2/10/2022.
+ */
+@Module
+@InstallIn(SingletonComponent::class)
+object TrackerDataModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient() = OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor().apply {
+            setLevel(HttpLoggingInterceptor.Level.BODY)
+        })
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideOpenFoodApi(client: OkHttpClient): OpenFoodApi {
+        return Retrofit.Builder().apply {
+            baseUrl(OpenFoodApi.BASE_URL)
+            addConverterFactory(MoshiConverterFactory.create())
+            client(client)
+        }
+            .build()
+            .create()
+    }
+}
+
