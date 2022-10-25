e8f27a553fbf0a5653bd93b63f974691843a02e1
Mahmoud H. Alim <mahmoudhusseinalim@gmail.com>
2022-10-25 13:50:24 +0200

    add new clickableNoRippleC() Modifier Ext to disable ripple to be used with other modifier clickable effects

---
+++ onboarding/onboarding_presentation/src/main/java/com/mahmoudalim/onboarding_presentation/composables/NoRippleClickable.kt
@@ -0,0 +1,19 @@
+package com.mahmoudalim.onboarding_presentation.composables
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+
+/**
+ * @author Mahmoud Alim on 25/10/2022.
+ */
+
+fun Modifier.clickableNoRippleC(onClick:() -> Unit) = composed {
+    this.clickable(
+        interactionSource = remember { MutableInteractionSource() },
+        indication = null,
+        onClick = { onClick() }
+    )
+}
